7d680394289d4222a21b5341d75a3b7b4f2fe1a2
Abhinav Dixit <abhinavdixit@192.168.1.15>
2024-04-20 10:27:24 +0530

    variables and scoping

---
+++ 02_Variables_Scope.js
@@ -0,0 +1,40 @@
+/*
+var varNum = 0;
+let letNum = 0;
+
+console.log("varNum", varNum);
+console.log("letNum", letNum);
+
+*/
+
+// hoisting....
+
+
+
+console.log("varNum", varNum);  // undefined
+console.log("letNum", letNum); // reference error 
+
+
+
+var varNum = 0;   
+let letNum = 0;
+
+
+
+
+
+function test(){
+    if(true){
+        var varNum = 0;
+        let letNum = 0;
+    }
+    console.log('varNum', varNum);  // 0
+    console.log('letNum', letNum); // Ref error
+}
+
+
+const constNum = 0;
+
+constNum = 3;  // error => cant re-assign
+
+// but we can mutate... like arr.push... in const arr;
