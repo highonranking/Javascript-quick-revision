59313f3d3d03d32b88c3e4e07b1afd8f0cfed9cd
Abhinav Dixit <abhinavdixit@192.168.1.12>
2024-04-23 18:46:38 +0530

    Closures examples

--- 13_ Closures.js
+++ 13_ Closures.js
@@ -155,6 +155,37 @@ If you try this code out, you'll see that it doesn't work as expected. No matter
 The reason for this is that the functions assigned to onfocus form closures; they consist of the function definition and the captured environment from the setupHelp function's scope. Three closures have been created by the loop, but each one shares the same single lexical environment, which has a variable with changing values (item). This is because the variable item is declared with var and thus has function scope due to hoisting. The value of item.help is determined when the onfocus callbacks are executed. Because the loop has already run its course by that time, the item variable object (shared by all three closures) has been left pointing to the last entry in the helpText list.
 */
 
+//One solution in this case is to use more closures:
+
+function showHelp(help) {
+    document.getElementById("help").textContent = help;
+  }
+  
+  function makeHelpCallback(help) {
+    return function () {
+      showHelp(help);
+    };
+  }
+  
+  function setupHelp() {
+    var helpText = [
+      { id: "email", help: "Your email address" },
+      { id: "name", help: "Your full name" },
+      { id: "age", help: "Your age (you must be over 16)" },
+    ];
+  
+    for (var i = 0; i < helpText.length; i++) {
+      var item = helpText[i];
+      document.getElementById(item.id).onfocus = makeHelpCallback(item.help);
+    }
+  }
+  
+  setupHelp();
+
+  
+  //This works as expected. Rather than the callbacks all sharing a single lexical environment, the makeHelpCallback function creates a new lexical environment for each callback, in which help refers to the corresponding string from the helpText array.
+
+  
 //other way to write the above using anonymous closures is:
 
 
